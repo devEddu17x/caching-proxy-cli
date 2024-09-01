4f1fa70497ef4c8eb6246fa242857500200fced9
Eddu <azabacheeduardo85@gmail.com>
2024-09-01 17:02:44 -0500

    feat: Add server.js file for handling requests from the real server

---
+++ server.js
@@ -0,0 +1,15 @@
+import * as http from 'node:http'
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify( {
+        message: 'Request has been received from real server',
+        method: req.method,
+        url: req.url,
+        }));
+})
+
+
+server.listen(3000, () => {
+    console.log(`Server is running on http://localhost:3000`);           
+})
