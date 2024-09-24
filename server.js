daf7b10f4ce4816f8186f9f8293d927c4991efce
Eddu <azabacheeduardo85@gmail.com>
2024-09-24 10:35:26 -0500

    refactor: Update server.js to improve error handling and code structure

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 import * as http from 'node:http'
 
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify( {
-        message: 'Request has been received from real server',
-        method: req.method,
-        'url-in-real-server': req.url,
-        }));
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify({
+    message: 'Request has been received from real server',
+    method: req.method,
+    'url-in-real-server': req.url
+  }))
 })
 
-
 server.listen(3000, () => {
-    console.log(`Server is running on http://localhost:3000`);           
+  console.log('Server is running on http://localhost:3000')
 })
